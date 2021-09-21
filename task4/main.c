07ccd716a516f48fa3440c1170ad467651587268
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-09-21 14:25:25 +0300

    Added drawing for the counter

--- task4/main.c
+++ task4/main.c
@@ -7,6 +7,16 @@
 #include "gc9a01.h"
 #include "buttons_module.h"
 
+#define CONVERSION_BUFFER_SIZE 64
+
+typedef struct drawing_processor
+{
+	bool counter_rect_invalidated;
+}drawing_processor_t;
+
+drawing_processor_t drawing_manager = { .counter_rect_invalidated = true };
+
+
 int counter_value = 0;
 
 void push_counter_up()
@@ -28,8 +38,10 @@ void reset_counter()
 	printf("Updated counter value RESET:%d\n",counter_value);
 	update_counter_rect();
 }
+
 void update_counter_rect()
 {
+	drawing_manager.counter_rect_invalidated = true;
 }
 
 void draw_current_time()
@@ -38,8 +50,19 @@ void draw_current_time()
 void draw_ip_address()
 {
 }
+
 void draw_counter_value()
 {
+	if(drawing_manager.counter_rect_invalidated)
+	{
+		const int x0_counter = LCD_WIDTH / 2 - Font_16x26.width/2;
+		const int y0_counter = LCD_HEIGHT /2 - Font_16x26.height/2;
+		char conversion_buffer[CONVERSION_BUFFER_SIZE];
+		snprintf(conversion_buffer, sizeof(conversion_buffer), "%d", counter_value);
+		lcd_put_string(conversion_buffer,x0_counter,y0_counter,Font_16x26,COLOR_RED,COLOR_BLACK);
+		lcd_update_screen();
+		drawing_manager.counter_rect_invalidated = false;
+	}
 }
 
 int main(int argc, char *argv[])
@@ -59,6 +82,7 @@ int main(int argc, char *argv[])
 	attach_short_click_listener_counter_up(&push_counter_up);
 	attach_short_click_listener_counter_down(&push_counter_down);
 	attach_short_click_listener_control_button(&reset_counter);
+
 	while (true) {
 		process_buttons_module();
 		draw_counter_value();
