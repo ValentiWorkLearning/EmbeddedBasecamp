4a5d0b1e678711c3c977923beff1a5a9cf5e08c0
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-09-29 01:15:31 +0300

    Added gpio module integration

--- final_task/display_driver/gpio_module.c
+++ final_task/display_driver/gpio_module.c
@@ -4,25 +4,35 @@
 // https://01.org/linuxgraphics/gfx-docs/drm/driver-api/gpio/driver.html
 // https://android.googlesource.com/kernel/msm/+/android-msm-flo-3.4-kitkat-mr0/drivers/watchdog/wm831x_wdt.c
 // https://lwn.net/Articles/532714/
+
+#define FORMAT_BUFFER_SIZE 64
+static char format_buffer[FORMAT_BUFFER_SIZE];
+
 int gpio_module_init_out(unsigned int gpio)
 {
-	return 0;
+	snprintf(format_buffer, FORMAT_BUFFER_SIZE, "GPIO_%d", gpio);
+	gpio_request(gpio, format_buffer);
+
+	return gpio_direction_output(gpio, 0);
 }
 
 int gpio_module_init_in(unsigned int gpio)
 {
-	return 0;
+	snprintf(format_buffer, FORMAT_BUFFER_SIZE, "GPIO_%d", gpio);
+	gpio_request(gpio, format_buffer);
+
+	return gpio_direction_input(gpio);
 }
 
-int gpio_module_free(unsigned int gpio)
+void gpio_module_free(unsigned int gpio)
 {
-	return 0;
+	gpio_free(gpio);
 }
-int gpio_module_set_value(unsigned int gpio, unsigned int value)
+void gpio_module_set_value(unsigned int gpio, unsigned int value)
 {
-	return 0;
+	gpio_set_value(gpio, value);
 }
 int gpio_module_get_value(unsigned int gpio)
 {
-	return 0;
+	return gpio_get_value(gpio);
 }
--- final_task/display_driver/gpio_module.h
+++ final_task/display_driver/gpio_module.h
@@ -3,7 +3,7 @@
 
 int gpio_module_init_out(unsigned int gpio);
 int gpio_module_init_in(unsigned int gpio);
-int gpio_module_free(unsigned int gpio);
-int gpio_module_set_value(unsigned int gpio, unsigned int value);
+void gpio_module_free(unsigned int gpio);
+void gpio_module_set_value(unsigned int gpio, unsigned int value);
 int gpio_module_get_value(unsigned int gpio);
 #endif
