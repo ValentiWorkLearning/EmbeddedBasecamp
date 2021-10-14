38265db3e0cd7d9196ab1e9e7e0fccda4d82de47
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-10-14 15:59:13 +0100

    Added function for obtaining current temperature

--- exercise7/time_script.sh
+++ exercise7/time_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#-x
 
 readonly RTC_ADDRESS=0x68
 readonly RTC_TEMP_UPPER_BYTE=0x11
@@ -9,20 +10,33 @@ currentTimeMin=0
 currentTimeSec=0
 currentTemperature=0
 
-function detectDevicesOnBus()
-{
+detectDevicesOnBus () {
 
 	echo "-------------Detecting i2c devices on the i2c bus 1----------------"
 	i2cdetect -y 1
 	echo "-------------Devices detection has been completed------------------"
 }
 
-function readCurrentTemperature()
-{
-	local tempUpper = i2cget -y 1 $(RTC_ADDRESS) $(RTC_TEMP_UPPER_BYTE)
-	local tempLower = i2cget -y 1 $(RTC_ADDRESS) $(RTC_TEMP_LOWER_BYTE)
-	echo "Temp upper:" $(tempUpper)
-	echo "Temp lower:" $(tempLower)
+readCurrentTemperature () {
+	tempUpper=`i2cget -y 1 $RTC_ADDRESS $RTC_TEMP_UPPER_BYTE`
+	tempLower=`i2cget -y 1 $RTC_ADDRESS $RTC_TEMP_LOWER_BYTE`
+	let tempFraction=$((tempLower>>6))
+	let tempFractionConverted=0
+	case "$tempFraction" in
+		0 )
+			tempFractionConverted="0"
+		;;
+		1 )
+			tempFractionConverted="25"
+		;;
+		2 )
+			tempFractionConverted="50"
+		;;
+		3 )
+			tempFractionConverted="75"
+		;;
+	esac
+	printf "Temperature is: %d.%s \n" $tempUpper $tempFractionConverted 
 }
 
 detectDevicesOnBus
