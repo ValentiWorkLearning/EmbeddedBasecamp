1836763f12ae9f91f905df92e85048bfbf216701
Valentyn Korniienko <kornienko-vr@rambler.ru>
2021-10-14 15:25:09 +0100

    Added draft script version

--- exercise7/time_script.sh
+++ exercise7/time_script.sh
@@ -1,6 +1,29 @@
+#!/bin/bash
+
+readonly RTC_ADDRESS=0x68
+readonly RTC_TEMP_UPPER_BYTE=0x11
+readonly RTC_TEMP_LOWER_BYTE=0x12
+
 currentTimeHour=0
 currentTimeMin=0
 currentTimeSec=0
+currentTemperature=0
 
-echo "Detecting i2c devices on the i2c bus 0"
-i2detect -y 0
+function detectDevicesOnBus()
+{
+
+	echo "-------------Detecting i2c devices on the i2c bus 1----------------"
+	i2cdetect -y 1
+	echo "-------------Devices detection has been completed------------------"
+}
+
+function readCurrentTemperature()
+{
+	local tempUpper = i2cget -y 1 $(RTC_ADDRESS) $(RTC_TEMP_UPPER_BYTE)
+	local tempLower = i2cget -y 1 $(RTC_ADDRESS) $(RTC_TEMP_LOWER_BYTE)
+	echo "Temp upper:" $(tempUpper)
+	echo "Temp lower:" $(tempLower)
+}
+
+detectDevicesOnBus
+readCurrentTemperature
